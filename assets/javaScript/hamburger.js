78f4fbbcd4fda49f2ce4c59d16b9b94a6db99b10
msms <msr.warden@gmail.com>
2024-01-17 22:23:07 +0900

    commit

--- assets/javaScript/hamburger.js
+++ assets/javaScript/hamburger.js
@@ -6,22 +6,3 @@ $(function(){
     // $('body').toggleClass('close','.slow');
   });
 });
-
-// $(function(){
-//   $('.p-hamburger__button').click(function(){
-//     $('.barTop, .barMiddle, .barBottom').toggleClass('open');
-//     $('.p-hamburger').toggleClass('is-active'),2000;
-//     $('.wrapper').toggleClass('close'),2000;
-//   });
-// });
-
-
-// $(function(){
-//   $('.p-hamburger__button').click(function(){
-//     $('.barTop, .barMiddle, .barBottom').toggleClass('open');
-//     $('header,main,footer').toggleClass('close');
-//   });
-//   if($('header,main,footer').hasClass('close')){
-//     $('header,main,footer').addClass('fixed')
-//   }
-// });
