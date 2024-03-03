23aabc9ed3cdfb22a15524300dbe5e3ac0731114
msms <msr.warden@gmail.com>
2024-03-03 22:20:52 +0900

    new commit

--- assets/javaScript/hamburger.js
+++ assets/javaScript/hamburger.js
@@ -1,8 +1,13 @@
 $(function(){
   $('.p-hamburger__button').click(function(){
+console.log('クリックしたよ');
+
     $('.barTop, .barMiddle, .barBottom').toggleClass('open');
     $('.p-hamburger').toggleClass('is-active',);
     $('.wrapper').toggleClass('close','.slow');
     // $('body').toggleClass('close','.slow');
+
+    document.querySelector('html').style.overflow = 'hidden';
+    document.querySelector('body').style.overflow = 'hidden';
   });
 });
